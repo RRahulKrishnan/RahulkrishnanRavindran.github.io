8fda1138a5fb9d200cc847adf45cd994de7512d2
Rahulkrishnan Ravindran <87916269+RRahulKrishnan@users.noreply.github.com>
2022-09-16 04:37:58 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -24,14 +24,12 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'https://rrahulkrishnan.github.io/RahulkrishnanRavindran.github.io/',
+    'username' => 'smartwaterbottleinc@gmail.com',
+    'password' => 'Bharat@12',
     'port' => '587'
   );
-  */
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
